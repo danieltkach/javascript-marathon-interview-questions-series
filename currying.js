e923b24361458c363bd18cdebd178a310a6d6c23
Daniel Tkach <danieltkach@gmail.com>
2022-03-14 03:17:42 -0300

    currying

--- currying.js
+++ currying.js
@@ -1,2 +1,13 @@
-const applyTax =(percentage) => (amountSpent) => percentage * amountSpent / 100;
+const applyDiscount =(percentage) => (amountSpent) => amountSpent - (percentage * amountSpent / 100);
 
+const discount50 = applyDiscount(50);
+const discount10 = applyDiscount(10);
+
+let price = 38000;
+console.log('price: $', price);
+
+let finalPrice = discount50(price);
+console.log('50% off ->', finalPrice);
+
+finalPrice = discount10(price);
+console.log('10% off ->', finalPrice);
